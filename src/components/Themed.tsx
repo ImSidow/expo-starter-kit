b0f4af3adf711d671967363ea762c1003a339dfa
Asad Ali Sidow <73076630+ImSidow@users.noreply.github.com>
2022-09-27 05:54:54 +0300

    Update Themed.tsx
    updated

--- src/components/Themed.tsx
+++ src/components/Themed.tsx
@@ -1,17 +1,10 @@
-/**
- * Learn more about Light and Dark modes:
- * https://docs.expo.io/guides/color-schemes/
- */
+import { Text as DefaultText, View as DefaultView } from "react-native";
+import { SafeAreaView as DefaultSafeAreaView } from "react-native-safe-area-context";
 
-import { Text as DefaultText, View as DefaultView } from 'react-native';
+import Colors from "@helpers/constants/Colors";
+import useColorScheme from "@hooks/useColorScheme";
 
-import Colors from '../helpers/constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-
-export function useThemeColor(
-    props: { light?: string; dark?: string },
-    colorName: keyof typeof Colors.light & keyof typeof Colors.dark
-) {
+export function useThemeColor(props: { light?: string; dark?: string }, colorName: keyof typeof Colors.light & keyof typeof Colors.dark) {
     const theme = useColorScheme();
     const colorFromProps = props[theme];
 
@@ -27,22 +20,26 @@ type ThemeProps = {
     darkColor?: string;
 };
 
-export type TextProps = ThemeProps & DefaultText['props'];
-export type ViewProps = ThemeProps & DefaultView['props'];
+export type TextProps = ThemeProps & DefaultText["props"];
+export type ViewProps = ThemeProps & DefaultView["props"];
 
 export function Text(props: TextProps) {
     const { style, lightColor, darkColor, ...otherProps } = props;
-    const color = useThemeColor({ light: lightColor, dark: darkColor }, 'text');
+    const color = useThemeColor({ light: lightColor, dark: darkColor }, "text");
 
     return <DefaultText style={[{ color }, style]} {...otherProps} />;
 }
 
 export function View(props: ViewProps) {
     const { style, lightColor, darkColor, ...otherProps } = props;
-    const backgroundColor = useThemeColor(
-        { light: lightColor, dark: darkColor },
-        'background'
-    );
+    const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, "background");
 
     return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+export function SafeAreaView(props: ViewProps) {
+    const { style, lightColor, darkColor, ...otherProps } = props;
+    const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, "background");
+
+    return <DefaultSafeAreaView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
